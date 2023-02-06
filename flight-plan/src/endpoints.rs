e30a6fe283f1d59b66ce413e80f379afea360802
Pluralsight <pluralsight@noemail.com>
2023-02-05 21:30:00 -0500

    Refactored for module 5

--- flight-plan/src/endpoints.rs
+++ flight-plan/src/endpoints.rs
@@ -1,7 +1,15 @@
 use actix_web::{get, post, delete, put, HttpResponse, Responder, web};
 use crate::database;
 use crate::database::insert_flight_plan;
-use crate::schema::{FlightPlan};
+use crate::schema::{FlightPlan, User};
+
+#[post("/api/v1/admin/user/create")]
+pub async fn new_user(user: web::Json<User>) -> impl Responder {
+    match database::create_user(user.into_inner()) {
+        Ok(api_key) => HttpResponse::Ok().body(api_key),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string())
+    }    
+}
 
 #[get("/api/v1/flightplan")]
 pub async fn get_all_flight_plans() -> impl Responder {
--- flight-plan/src/main.rs
+++ flight-plan/src/main.rs
@@ -1,30 +1,89 @@
+mod endpoints;
 mod schema;
 mod database;
-mod endpoints;
 
-use actix_web::{App, HttpServer};
+use config::Config;
+use actix_web_httpauth::extractors::bearer::{BearerAuth, self};
+use actix_web_httpauth::extractors::AuthenticationError;
+use actix_web_httpauth::middleware::HttpAuthentication;
+use actix_web::{App, HttpServer, Error, dev::ServiceRequest};
 use env_logger::Env;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use actix_cors::Cors;
 use actix_web::middleware::Logger;
 
 use crate::endpoints::{get_all_flight_plans, get_flight_plan_by_id, 
                        delete_flight_plan_by_id, file_flight_plan, 
-                       update_flight_plan};
+                       update_flight_plan, new_user};
+
+async fn validator(
+    req: ServiceRequest,
+    credentials: BearerAuth
+) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+
+    let config = req.app_data::<bearer::Config>()
+            .cloned()
+            .unwrap_or_default()
+            .scope("urn:example:channel=HBO&urn:example:rating=G,PG-13");
+
+    match database::get_user(String::from(credentials.token())) {
+        Ok(user) => {
+            match user {
+                Some(_) => {
+                    return Ok(req);
+                },
+                None => {
+                    Err((AuthenticationError::from(config).into(), req))
+                },
+            }
+            
+        },
+        Err(_) => {
+            Err((AuthenticationError::from(config).into(), req))
+        }
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(Env::default().default_filter_or("info")); 
+    
+    let settings = Config::builder()
+        .add_source(config::File::with_name("config"))
+        .build()
+        .unwrap();
+
+    let certificate_key = settings.get_string("CERTIFICATE_KEY").unwrap();
+    let certificate = settings.get_string("CERTIFICATE").unwrap();
+
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder.set_private_key_file(certificate_key, SslFiletype::PEM).unwrap();
+    builder.set_certificate_chain_file(certificate).unwrap();
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));    
     HttpServer::new(move || {
+        let middleware = HttpAuthentication::bearer(validator);
         App::new()
             .service(get_flight_plan_by_id)
             .service(get_all_flight_plans)
             .service(delete_flight_plan_by_id)
             .service(file_flight_plan)
             .service(update_flight_plan)
+            .service(new_user)
+            .wrap(middleware)
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(
+                Cors::default()
+                    .allow_any_method()
+                    .allow_any_origin()
+                    .allow_any_header()
+                    .supports_credentials()
+                    .max_age(3600),
+            )
     })
         .bind(("0.0.0.0", 3000))?
-        .workers(2)
+        .bind_openssl("0.0.0.0:3001", builder)?
+        .workers(8)
         .run()
         .await
 }
