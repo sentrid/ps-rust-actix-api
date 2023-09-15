7ed5428dcded8ab7bfdbcabb98fcc317c9b18cf4
Pluralsight <pluralsight@noemail.com>
2023-09-14 20:15:06 -0400

    Updated endpoints.rs to match M4 and M5

--- flight-plan/src/endpoints.rs
+++ flight-plan/src/endpoints.rs
@@ -1,45 +1,37 @@
 use actix_web::{get, post, delete, put, HttpResponse, Responder, web};
 use crate::database;
-use crate::schema::{FlightPlan, User};
-
-#[post("/api/v1/admin/user/create")]
-pub async fn new_user(user: web::Json<User>) -> impl Responder {
-    match database::create_user(user.into_inner()) {
-        Ok(api_key) => HttpResponse::Ok().body(api_key),
-        Err(e) => HttpResponse::InternalServerError().body(e.to_string())
-    }    
-}
+use crate::schema::FlightPlan;
+use web::{Path, Json};
 
 #[get("/api/v1/flightplan")]
 pub async fn get_all_flight_plans() -> impl Responder {
     match database::get_all_flight_plans().unwrap() {
        Some(flight_plan_list) => {
-            return HttpResponse::Ok().content_type("application/json").json(flight_plan_list);
+            HttpResponse::Ok().content_type("application/json").json(flight_plan_list)
        },
        None => {
-            return HttpResponse::NoContent().body("There are no flight plans filed with this system");
+            HttpResponse::NoContent().body("There are no flight plans filed with this system")
        }
     }
 }
 
 #[get("/api/v1/flightplan/{flight_plan_id}")]
-pub async fn get_flight_plan_by_id(path: web::Path<String>) -> impl Responder {
-    let flight_plan_id = path.into_inner();
-    let db_result = database::get_flight_plan_by_id(flight_plan_id.clone()).unwrap();
+pub async fn get_flight_plan_by_id(id: Path<String>) -> impl Responder {
+    let flight_plan_id = id.into_inner();
     
-    match db_result {
+    match database::get_flight_plan_by_id(flight_plan_id.clone()).unwrap() {
         Some(flight_plan_from_db) => {
-            return HttpResponse::Ok().json(flight_plan_from_db);
+            HttpResponse::Ok().content_type("application/json").json(flight_plan_from_db)
         },
         None => {
-            return HttpResponse::NotFound().body(format!("There is not any flight plan with id {}", flight_plan_id));
+            HttpResponse::NotFound().body(format!("There is not any flight plan with id {}", flight_plan_id))
         }
     }
 }
 
 #[delete("/api/v1/flightplan/{flight_plan_id}")]
-pub async fn delete_flight_plan_by_id(path: web::Path<String>) -> impl Responder {
-    let flight_plan_id = path.into_inner();
+pub async fn delete_flight_plan_by_id(id: Path<String>) -> impl Responder {
+    let flight_plan_id = id.into_inner();
     match database::delete_flight_plan(flight_plan_id.clone()) {
         Ok(successful) => {
             if successful {
@@ -55,7 +47,7 @@ pub async fn delete_flight_plan_by_id(path: web::Path<String>) -> impl Responder
 }
 
 #[post("/api/v1/flightplan")]
-pub async fn file_flight_plan(flight_plan: web::Json<FlightPlan>) -> impl Responder {
+pub async fn file_flight_plan(flight_plan: Json<FlightPlan>) -> impl Responder {
     match database::insert_flight_plan(flight_plan.into_inner()) {
         Ok(_) => {
             HttpResponse::Ok().finish()
@@ -67,7 +59,7 @@ pub async fn file_flight_plan(flight_plan: web::Json<FlightPlan>) -> impl Respon
 }
 
 #[put("/api/v1/flightplan")]
-pub async fn update_flight_plan(flight_plan: web::Json<FlightPlan>) -> impl Responder {
+pub async fn update_flight_plan(flight_plan: Json<FlightPlan>) -> impl Responder {
     let updated_flight_plan = flight_plan.into_inner();
     match database::update_flight_plan(updated_flight_plan.clone()) {
         Ok(succeeded) => {
@@ -75,7 +67,7 @@ pub async fn update_flight_plan(flight_plan: web::Json<FlightPlan>) -> impl Resp
                 HttpResponse::Ok().finish()
             } else {
                 HttpResponse::NotFound().body(format!("There is not any flight plan with id {}", updated_flight_plan.flight_plan_id))
-            }            
+            }
         }
         Err(_) => {
             HttpResponse::InternalServerError().finish()
